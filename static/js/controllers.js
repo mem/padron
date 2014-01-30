a0332b414ecb9430b934fa64a70183e1f9e91836
Marcelo E. Magallon <marcelo.magallon@gmail.com>
2014-01-30 07:07:31 -0600

    Handle shorter/longer ids
        PMMMNNNN   => P0MMMNNNN
        PMMMNNN    => P0MMM0NNN
        0PMMMMNNNN => PMMMMNNNN
    
    yes, people have done all of this :-)

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -4,7 +4,31 @@ var padronApp = angular.module('padronApp', ['ui.bootstrap'])
 
 padronApp.controller('PadronCtrl', function ($scope, $http) {
   $scope.search = function() {
-    $http.get('persona/' + this.cedula).success(function(data) {
+    var cedula = this.cedula.trim();
+    if (cedula.match(/^\d+$/)) {
+      switch (cedula.length) {
+      case 7:
+        // PMMMNNN => P0MMM0NNNN
+        cedula = cedula.substring(0, 1)
+                 + "0" + cedula.substring(1, 4)
+                 + "0" + cedula.substring(4, 8);
+        break;
+      case 8:
+        // PMMMNNNN => P0MMMNNNN
+        cedula = cedula.substring(0, 1)
+                 + "0" + cedula.substring(1, 4)
+                 + cedula.substring(4, 8);
+        break;
+      case 9:
+        // PMMMMNNNN, nothing to do!
+        break;
+      case 10:
+        // 0PMMMMNNNN => PMMMMNNNN
+        cedula = cedula.substring(1);
+        break;
+      }
+    }
+    $http.get('persona/' + cedula).success(function(data) {
       $scope.personas = [ data ];
       $scope.found = 1;
     }).error(function() {
